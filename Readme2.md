ed6fbd38a60e6844cdb519f9e042cdf731068f1b
callisto28 <iotaseb@gmail.com>
2022-03-28 22:59:57 +0200

    FEAT[apollo + graphql] mise en place des fonctionnalité graphQl + apollo

---
+++ Readme2.md
@@ -0,0 +1,13 @@
+# Fonction de lancement de apollo
+
+Permet de lancer la génération du fichier graphQL
+
+```js 
+npm run gen-typings
+``` 
+-Permet de lancer nestJs
+
+```js
+npm run start:dev
+```
+Puis aller sur localhost:3000/graphql
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { AppService } from './app.service';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { Module } from '@nestjs/common';
 import { ArticlesModule } from './articles/articles.module';
+import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo'
+
 
 @Module({
-  imports: [GraphQLModule.forRoot({
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
     playground:false,
     plugins:[ApolloServerPluginLandingPageLocalDefault()],
-    typePaths: ['.src/**/*.graphql'],
+    typePaths: ['./src/**/*.graphql'],
   }), ArticlesModule],
   controllers: [AppController],
   providers: [AppService],
